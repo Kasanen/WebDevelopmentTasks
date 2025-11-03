c1a410e6c042d0ae19ee9d0fb17b3eb54f4cf942
Kasanen <veijok119@gamil.com>
2025-11-03 15:44:40 +0200

    Yksilotehtavat

--- Yksilotehtava/data.js
+++ Yksilotehtava/data.js
@@ -40,14 +40,14 @@ async function fetchMenu(id) {
     `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${id}/en`
   );
 
-  const menuItems = data.courses.map(item => item.name);
+  const menuItems = data.courses.map((item) => item.name);
   return menuItems;
 }
 
 async function fetchRestaurants() {
   const data = await getData(url);
 
-  const restaurants = data.map(r => ({
+  const restaurants = data.map((r) => ({
     _id: r._id,
     address: r.address,
     city: r.city,
@@ -56,12 +56,14 @@ async function fetchRestaurants() {
     phone: r.phone,
     postalCode: r.postalCode,
   }));
-  sorting(restaurants);
   return restaurants;
 }
 
-function sorting(list) {
-  list.sort((a, b) => {
+async function main() {
+  const restaurants = await fetchRestaurants();
+
+  // sorting
+  restaurants.sort((a, b) => {
     const nameA = a.name.toUpperCase();
     const nameB = b.name.toUpperCase();
     if (nameA < nameB) {
@@ -72,16 +74,12 @@ function sorting(list) {
     }
     return 0;
   });
-}
-
-async function main() {
-  const restaurants = await fetchRestaurants();
 
   // Printing out
   const table = document.querySelector('table');
   let trId = 0;
 
-  restaurants.map(restaurant => {
+  restaurants.map((restaurant) => {
     trId++;
     table.insertAdjacentHTML(
       'beforeend',
@@ -102,7 +100,7 @@ async function clicked(id) {
   const restaurants = await fetchRestaurants();
 
   const menu = await fetchMenu(restaurants[id - 1]._id);
-  const menuHTML = menu.map(item => `<li>${item}</li>`).join('');
+  const menuHTML = menu.map((item) => `<li>${item}</li>`).join('');
 
   const dialog = document.querySelector('dialog');
 
@@ -122,7 +120,7 @@ async function clicked(id) {
                 <li>City: ${restaurants[id - 1].city}</li>
                 <li>Phone number: ${restaurants[id - 1].phone}</li>
                 <li>Company: ${restaurants[id - 1].company}</li>
-                <h4> WEEKLY MENU </h4>
+                                <h4> WEEKLY MENU </h4>
                 ${menuHTML}
                 <h3>>>> Press ESC to close <<<</h3>
             </ul>
