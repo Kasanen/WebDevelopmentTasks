ed7b05ddc1d1612a20a7d1a1c409a4c6e82c5c8e
Kasanen <veijok119@gamil.com>
2025-11-16 15:56:36 +0200

    Yksilötehtävät Lisätty kartta ja filtteröinti nimellä + etäisyydellä

--- Yksilotehtava/data.js
+++ Yksilotehtava/data.js
@@ -1,5 +1,6 @@
 const url = 'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants';
 
+/* -- Base -- */
 async function getData(requestURL) {
   try {
     const response = await fetch(requestURL, {
@@ -37,14 +38,58 @@ async function getMenu(requestURL) {
 
 async function fetchMenu(id) {
   const data = await getMenu(
-    `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${id}/en`
+    `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${id}/fi`
   );
 
   const menuItems = data.courses.map((item) => item.name);
   return menuItems;
 }
 
-async function fetchRestaurants() {
+// User location
+function getUserLocation(callback) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      callback({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+}
+
+// Promise wrapper for geolocation
+function getUserLocationPromise() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      return reject(new Error('Geolocation not supported'));
+    }
+    navigator.geolocation.getCurrentPosition(
+      (pos) => resolve({lat: pos.coords.latitude, lon: pos.coords.longitude}),
+      (err) => reject(err)
+    );
+  });
+}
+
+// Calculating distance
+function getDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371;
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
+
+/* -- Fetch restaurants show -- */
+async function fetchRestaurants(filterNumb) {
   const data = await getData(url);
 
   const restaurants = data.map((r) => ({
@@ -55,12 +100,25 @@ async function fetchRestaurants() {
     name: r.name,
     phone: r.phone,
     postalCode: r.postalCode,
+    location: r.location,
   }));
-  sorting(restaurants);
+
+  switch (filterNumb) {
+    case 1:
+      await sortByName(restaurants);
+      break;
+    case 2:
+      await sortByLoc(restaurants);
+      break;
+    default:
+      await sortByName(restaurants);
+      console.log('Failed to filter by choice: ' + filterNumb);
+  }
   return restaurants;
 }
 
-function sorting(list) {
+// Sorting by name
+function sortByName(list) {
   list.sort((a, b) => {
     const nameA = a.name.toUpperCase();
     const nameB = b.name.toUpperCase();
@@ -74,11 +132,48 @@ function sorting(list) {
   });
 }
 
-async function main() {
-  const restaurants = await fetchRestaurants();
+// Sorting by location
+async function sortByLoc(list) {
+  try {
+    const loc = await getUserLocationPromise();
+    const userLat = loc.lat;
+    const userLon = loc.lon;
+
+    list.sort((a, b) => {
+      const aLat = a.location.coordinates[1];
+      const aLon = a.location.coordinates[0];
+      const bLat = b.location.coordinates[1];
+      const bLon = b.location.coordinates[0];
+
+      const distA = getDistance(userLat, userLon, aLat, aLon);
+      const distB = getDistance(userLat, userLon, bLat, bLon);
+      return distA - distB;
+    });
+  } catch (err) {
+    console.error(
+      'Could not get user location, falling back to name sort:',
+      err
+    );
+    sortByName(list);
+  }
+}
+
+async function main(filterNumb) {
+  const restaurants = await fetchRestaurants(filterNumb);
 
   // Printing out
   const table = document.querySelector('table');
+  table.innerHTML = '';
+  table.insertAdjacentHTML(
+    'beforeend',
+    `
+              <tr id="table-top">
+                <th>Nimi</th>
+                <th>Osoite</th>
+              </tr>
+        `
+  );
+
   let trId = 0;
 
   restaurants.map((restaurant) => {
@@ -86,7 +181,7 @@ async function main() {
     table.insertAdjacentHTML(
       'beforeend',
       `
-            <tr id="${trId}" onclick="clicked(this.id)" >
+            <tr id="${trId}" onclick="clicked(this.id)" style="cursor: pointer">
                 <td>${restaurant.name}</td>
                 <td>${restaurant.address}</td>
             </tr>
@@ -95,7 +190,7 @@ async function main() {
   });
 }
 
-main();
+main(1);
 
 // Clickable
 async function clicked(id) {
@@ -104,29 +199,93 @@ async function clicked(id) {
   const menu = await fetchMenu(restaurants[id - 1]._id);
   const menuHTML = menu.map((item) => `<li>${item}</li>`).join('');
 
-  const dialog = document.querySelector('dialog');
+  const dialog = document.getElementById('Modal2');
+  const modalText = document.getElementById('modalText');
 
   for (let i = 1; i < restaurants.length; i++) {
     document.getElementById(i).classList.remove('highlight');
   }
   document.getElementById(id).classList.add('highlight');
 
-  dialog.innerHTML = '';
-  dialog.insertAdjacentHTML(
+  modalText.innerHTML = '';
+  modalText.insertAdjacentHTML(
     'beforeend',
     `
+    <span class="closeModal" onclick="closeModal2()">&times;</span>
             <ul>
-                <li>Restaurant name: ${restaurants[id - 1].name}</li>
-                <li>Address: ${restaurants[id - 1].address}</li>
-                <li>Postal code: ${restaurants[id - 1].postalCode}</li>
-                <li>City: ${restaurants[id - 1].city}</li>
-                <li>Phone number: ${restaurants[id - 1].phone}</li>
-                <li>Company: ${restaurants[id - 1].company}</li>
-                                <h4> WEEKLY MENU </h4>
+                <li>Ravintolan nimi: ${restaurants[id - 1].name}</li>
+                <li>Osoite: ${restaurants[id - 1].address}</li>
+                <li>Postinumero: ${restaurants[id - 1].postalCode}</li>
+                <li>Kaupunki: ${restaurants[id - 1].city}</li>
+                <li>Puhelin numero: ${restaurants[id - 1].phone}</li>
+                <li>Yritys: ${restaurants[id - 1].company}</li>
+                <li>Suosikki:
+                <input type="checkbox" id="favorite" checked>
+                </li>
+                                <h4> Päivän Menu </h4>
                 ${menuHTML}
-                <h3>>>> Press ESC to close <<<</h3>
             </ul>
         `
   );
-  dialog.showModal();
+  modalText.parentElement.style.display = 'block';
+  document.documentElement.classList.add('modal-open');
 }
+
+/* -- MAP -- */
+async function mapOutput() {
+  const restaurants = await fetchRestaurants(1);
+
+  getUserLocation((loc) => {
+    const userLat = loc.lat;
+    const userLon = loc.lon;
+
+    var map = L.map('map').setView([userLat, userLon], 12);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    const redDivIcon = L.divIcon({
+      className: 'user-marker',
+      html: '<div class="dot"></div>',
+      iconSize: [20, 20],
+    });
+
+    getUserLocation((loc) => {
+      L.marker([userLat, userLon], {icon: redDivIcon}).addTo(map);
+    });
+
+    restaurants.map((restaurant) => {
+      let marker = L.marker([
+        restaurant.location.coordinates[1],
+        restaurant.location.coordinates[0],
+      ]).addTo(map);
+      marker.bindPopup(
+        '<h3>' + restaurant.name + '</h3><p>' + restaurant.address + '</p>'
+      );
+    });
+  });
+}
+
+mapOutput();
+
+/* Modal for data */
+var dataModal = document.getElementById('Modal2');
+
+function openModal() {
+  dataModal.style.display = 'block';
+  document.documentElement.classList.add('modal-open');
+}
+
+function closeModal2() {
+  dataModal.style.display = 'none';
+  document.documentElement.classList.remove('modal-open');
+}
+
+window.onclick = function (event) {
+  if (event.target == dataModal) {
+    closeModal2();
+  }
+};
